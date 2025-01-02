4ccc6bee89182317b70781491390cb58af1e6527
Y.Nanthachai <thecoachingonline@gmail.com>
2025-01-02 05:30:33 +0000

    Hello

---
+++ hello.py
@@ -0,0 +1,13 @@
+import strawberryfields as sf
+from strawberryfields import ops
+
+# create a 3-mode quantum program
+prog = sf.Program(3)
+
+with prog.context as q:
+    ops.Sgate(0.54) | q[0]
+    ops.Sgate(0.54) | q[1]
+    ops.Sgate(0.54) | q[2]
+    ops.BSgate(0.43, 0.1) | (q[0], q[2])
+    ops.BSgate(0.43, 0.1) | (q[1], q[2])
+    ops.MeasureFock() | q
